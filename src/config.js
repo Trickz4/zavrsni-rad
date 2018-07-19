b64c7de60c23109bd31171cf1d2d55112a3ec26a
Monika Buzuk <monikabuzuk@hotmail.com>
2018-07-19 23:10:52 +0200

    Zavrsni rad init

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBHNSYlhqUUnZiVaASmX-fz5pLzYyJs9cU",
+    authDomain: "zadnji-1.firebaseapp.com",
+    databaseURL: "https://zadnji-1.firebaseio.com",
+    projectId: "zadnji-1",
+    storageBucket: "zadnji-1.appspot.com",
+    messagingSenderId: "1068152427232"
+  };
+  
